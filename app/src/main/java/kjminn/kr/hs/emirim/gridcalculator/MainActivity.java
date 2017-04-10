83479e8319aa9535a127e697e8a542214ab812d6
Kyoungju Minn <kjminn2001@gmail.com>
2017-04-10 20:41:14 +0900

    #2 Event Programming 1

--- app/src/main/java/kjminn/kr/hs/emirim/gridcalculator/MainActivity.java
+++ app/src/main/java/kjminn/kr/hs/emirim/gridcalculator/MainActivity.java
@@ -2,18 +2,31 @@ package kjminn.kr.hs.emirim.gridcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edit1, edit2;
     Button[] butNums=new Button[10];
     Button[] butOps=new Button[4];
+    public static final  int SELECT_EDIT1=0;
+    public static final  int SELECT_EDIT2=1;
+    int selectEdit=SELECT_EDIT1;
+    String numStr="";
 //    int[] ids={R.id.but_num1,R.id.but_num2,R.id.but_num3,R.id.but_num4,R.id.but_num5,
 //               R.id.but_num6,R.id.but_num7,R.id.but_num8,R.id.but_num9,R.id.but_num10 };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edit1=(EditText)findViewById(R.id.edit1);
+        edit2=(EditText)findViewById(R.id.edit2);
+        edit1.setOnTouchListener(editHandler);
+        edit2.setOnTouchListener(editHandler);
+
 
         for(int i=0;i<butNums.length;i++){
 //            butNums[i]=(Button)findViewById(ids[i]);
@@ -30,7 +43,17 @@ public class MainActivity extends AppCompatActivity {
     View.OnClickListener butNumHandler=new View.OnClickListener() {
         @Override
         public void onClick(View view) {
+            Button but=(Button)view;
+            numStr+=but.getText();
+            switch (selectEdit){
+                case SELECT_EDIT1:
+                    edit1.setText(numStr);
+                    break;
+                case SELECT_EDIT2:
+                    edit2.setText(numStr);
+                    break;
 
+            }
         }
     };
 
@@ -38,6 +61,37 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View view) {
 
+        }
+    };
+
+/*    View.OnFocusChangeListener editHandler=new View.OnFocusChangeListener() {
+        @Override
+        public void onFocusChange(View view, boolean b) {
+            numStr="";
+            switch (view.getId()){
+                case R.id.edit1:
+                    selectEdit=SELECT_EDIT1;
+                    break;
+                case R.id.edit2:
+                    selectEdit=SELECT_EDIT2;
+                    break;
+            }
+        }
+    };*/
+
+    View.OnTouchListener editHandler=new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            numStr="";
+            switch (view.getId()){
+                case R.id.edit1:
+                    selectEdit=SELECT_EDIT1;
+                    break;
+                case R.id.edit2:
+                    selectEdit=SELECT_EDIT2;
+                    break;
+            }
+            return true;
         }
     };
 }
